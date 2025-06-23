2d7f1b5f35cb88f545db3c441ae766abd6a512e0
Dr. Hayder-Iraq <hayder.subhi@gmail.com>
2025-06-23 20:03:35 +0000

    نفس المشكلة

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
 
 import type { Metadata } from 'next';
-import { Tajawal } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from "@/components/ui/toaster"
-
-const tajawal = Tajawal({
-  subsets: ['arabic', 'latin'],
-  weight: ['400', '500', '700', '900'],
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: 'Iraqi Budgeteer | كي - راقب مصروفك',
@@ -22,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" className={tajawal.className} suppressHydrationWarning>
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body className={'antialiased'}>
         <ThemeProvider
           attribute="class"
