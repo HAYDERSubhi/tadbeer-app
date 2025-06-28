9bb473d4ef8319411764f51e66329ff1377995d2
Dr. Hayder-Iraq <hayder.subhi@gmail.com>
2025-06-28 15:02:20 +0000

    هنالك وقت 1-2 ثانية ياخذه التطبيق عند الانتقال بين التبويبات

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -5,7 +5,13 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+      },
+    },
+  }));
 
   return (
     <QueryClientProvider client={queryClient}>
