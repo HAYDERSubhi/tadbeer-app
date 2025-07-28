bf5a8c79084afacac1750efc1434b7f1f1ca105b
Dr. Hayder-Iraq <hayder.subhi@gmail.com>
2025-07-28 12:32:49 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 
 import type {NextConfig} from 'next';
-import withPWA from 'next-pwa';
 
-const pwaConfig = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
@@ -35,4 +34,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default pwaConfig(nextConfig);
+export default withPWA(nextConfig);
