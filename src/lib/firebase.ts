9745d13b9650493b928dcc263a0b8480e8e62d0c
Dr. Hayder-Iraq <hayder.subhi@gmail.com>
2025-08-03 19:56:17 +0000

    عند الضغط على كوكل ظهرت صفحة وفيها شريط يشير الى فاير ستوديو وهو شي لا ا

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getAuth, type Auth, GoogleAuthProvider } from "firebase/auth";
 // It's recommended to store these values in environment variables.
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "tadbeer.app", // Use custom domain for auth
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
